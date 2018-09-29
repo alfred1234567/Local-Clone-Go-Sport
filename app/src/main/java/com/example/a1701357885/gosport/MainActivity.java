2def53a84ec8092af8f7ad3b17caf42b52117704
alfred1234567 <43566272+alfred1234567@users.noreply.github.com>
2018-09-29 15:02:39 +0700

    Go Sport

---
+++ app/src/main/java/com/example/a1701357885/gosport/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.a1701357885.gosport;
+
+import android.annotation.SuppressLint;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+        mToggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
